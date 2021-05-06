a9d41ad4f81e4877b0e19d5493aa88ce60c7dd08
Edgar Haond <edgar.haond@gmail.com>
2021-05-06 15:25:34 +0200

    import du dataset et ajout du gitignore

--- edgar.py
+++ edgar.py
@@ -1,1 +1,11 @@
-print("Hello !")
+import json
+import pandas as pd
+
+# Opening JSON file
+tweets = []
+for line in open('data/raw0.json', 'r'):
+    tweets.append(json.loads(line))
+
+df = pd.DataFrame(tweets)
+
+print(df.head())
