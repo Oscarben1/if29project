4ca228584117e6e3d20d7ad068293ad89f47ac2b
Edgar Haond <edgar.haond@gmail.com>
2021-05-06 15:54:33 +0200

    boucle pour fichiers raw

--- edgar.py
+++ edgar.py
@@ -3,9 +3,11 @@ import pandas as pd
 
 # Opening JSON file
 tweets = []
-for line in open('data/raw0.json', 'r'):
-    tweets.append(json.loads(line))
+for i in range(10):
+  for line in open('data/raw'+ str(i) +'.json', 'r', encoding='utf8'):
+      tweets.append(json.loads(line))
 
 df = pd.DataFrame(tweets)
 
-print(df.head())
+#print(df.head())
+print(df.describe())
