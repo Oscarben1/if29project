2266db030acb8a49fa0d50c094a3dfdf3d7168d9
Oscarben1 <71690956+Oscarben1@users.noreply.github.com>
2021-05-07 18:32:18 +0200

    recupération de l'indicateur follower et l'indicateur friends et j'ai établi un nouvel indicateur qui est le ratio. J'ai essayé de récupérer la longueur des tweets mais ils font tous 32 cractères jsp pq

--- oscar.py
+++ oscar.py
@@ -14,12 +14,37 @@ print(df.head())
 print(df.describe())
 userdf = df['user']
 indicateurDfFriendsCount = np.zeros(len(userdf))
+indicateurDfFollowersCount = np.zeros(len(userdf))
+indicateurDfRatio = np.zeros(len(userdf))
 
 for i in range(len(userdf)):
     for key, value in userdf[i].items():
         if (key == "friends_count"):
             #print(key, value)
             indicateurDfFriendsCount[i] = value
+        elif (key == "followers_count"):
+            indicateurDfFollowersCount[i] = value
 
-print(indicateurDfFriendsCount)
+for i in range(len(indicateurDfFollowersCount)):
+    if (indicateurDfFriendsCount[i] != 0):
+        indicateurDfRatio[i] = indicateurDfFollowersCount[i] / indicateurDfFriendsCount[i]
+    else:
+        indicateurDfRatio[i] = indicateurDfFollowersCount[i]
 
+print(indicateurDfRatio)
+
+tweetsText = np.array([])
+
+for i in range(len(df)):
+   tweetsText = np.append(tweetsText, "")
+
+dfText = df['text']
+
+for i in range(len(df)):
+    tweetsText[i] = dfText[i]
+
+indicateurDfLongueurTweets = np.zeros(len(df))
+for i in range(len(df)):
+    indicateurDfLongueurTweets[i] = len(tweetsText[i])
+
+print(indicateurDfLongueurTweets)
