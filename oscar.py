d3d58b602febe8db358039cf6ab78dbcabae2d96
Oscarben1 <71690956+Oscarben1@users.noreply.github.com>
2021-05-14 19:04:48 +0200

    indicateur nb urls, nb hashtags et indicateur agressivite en se basant sur papier de recherche SPOT

--- oscar.py
+++ oscar.py
@@ -10,6 +10,7 @@ for i in range(2):
 
 df = pd.DataFrame(tweets)
 
+
 print(df.head())
 print(df.describe())
 userdf = df['user']
@@ -44,7 +45,64 @@ for i in range(len(df)):
     tweetsText[i] = dfText[i]
 
 indicateurDfLongueurTweets = np.zeros(len(df))
+
 for i in range(len(df)):
     indicateurDfLongueurTweets[i] = len(tweetsText[i])
 
-print(indicateurDfLongueurTweets)
+entitiesdf = df['entities']
+indicateurNbHashtags = np.zeros(len(userdf))
+
+for i in range(len(entitiesdf)):
+    indicateurNbHashtags[i] = len(entitiesdf[i]['hashtags'])
+
+indicateurNbURLs = np.zeros(len(userdf))
+for i in range(len(entitiesdf)):
+    indicateurNbURLs[i] = len(entitiesdf[i]['urls'])
+
+#Aggressiveness
+# "users" stockera les données nécessaire au calcul de l'agressivité pour chaque profile
+users = {}
+# On execute la requête et pour chacun des tweets, on conserve les données qui nous intéressent
+for i in range(len(df)):
+    # Si l'utilisateur n'a pas encore été rencontré, on l'ajoute à notre dictionnaire users
+    if df.iloc[i, :]['user'].get('id') not in users:
+        users[df.iloc[i, :]['user'].get('id')] = {}
+        users[df.iloc[i, :]['user'].get('id')]['timestamp'] = []
+        users[df.iloc[i, :]['user'].get('id')]['outgoinglinks'] = []
+        users[df.iloc[i, :]['user'].get('id')]['outgoinglinks'].append(len(df.iloc[i, :]['entities'].get('urls')))
+        users[df.iloc[i, :]['user'].get('id')]['timestamp'].append(int(df.iloc[i, :]['timestamp_ms']))
+
+    # Sinon on ajoute l'id d'utilisateur au dictionnaire users
+    else:
+        users[df.iloc[i, :]['user'].get('id')]['timestamp'].append(int(df.iloc[i, :]['timestamp_ms']))
+        users[df.iloc[i, :]['user'].get('id')]['outgoinglinks'].append(len(df.iloc[i, :]['entities'].get('urls')))
+
+print("Parcours des utilisateurs fini")
+
+# On détermine l'agressivité de chacun des profiles
+for user in users:
+    users[user].get('timestamp').sort(reverse=True)
+    timestamp = users[user].get('timestamp')
+    users[user].get('outgoinglinks').sort(reverse=True)
+    outgoinglinks = users[user].get('outgoinglinks')
+    users[user]['frequenceTweet'] = 1
+    users[user]['frequenceFriends'] = 0
+    nbOutgoingLinks = 0
+    # S'il y a plus d'un tweet de retenu, on additionne les écarts entre chaque tweet
+    if len(timestamp) > 1:
+        diffTime = timestamp[0] - timestamp[len(timestamp) - 1]
+        # On convertit le temps en h
+        diffTime = diffTime / float(3600000) + 0.000000000000001
+        users[user]['frequenceTweet'] = len(timestamp) / diffTime
+        for i in range(len(outgoinglinks)):
+            nbOutgoingLinks += outgoinglinks[i]
+        users[user]['frequenceFriends'] = nbOutgoingLinks / diffTime
+
+    users[user]['agressivité'] = (users[user]['frequenceFriends'] + users[user]['frequenceTweet']) / 350
+
+indicateurAgressivite = np.array([])
+
+for user in users:
+    indicateurAgressivite = np.append(indicateurAgressivite, users[user]['agressivité'])
+
+print(indicateurAgressivite)
